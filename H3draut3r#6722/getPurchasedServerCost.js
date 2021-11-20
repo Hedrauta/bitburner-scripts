df2f95efaeba86c486c7b2e9abb7e5e3db49854d
H3draut3r <H3draut3r@hotmail.de>
2021-11-20 21:12:09 +0100

    minor Adjustments

--- H3draut3r#6722/getPurchasedServerCost.js
+++ H3draut3r#6722/getPurchasedServerCost.js
@@ -1,8 +1,22 @@
 export async function main(ns) {
-    if (ns.args[0] != null && Number.isInteger(ns.args[0]) && ns.args[0]%2==0) {
-        ns.tprint("Costs for a new Server with "+ns.args[0]+"GB RAM: "+ns.getPurchasedServerCost(ns.args[0]));
+    if (ns.args[0] != null && Number.isInteger(ns.args[0])&&ns.args[0]>=1) {
+        let power = Math.pow(2, ns.args[0]);
+        let byte = "GB";
+        if (power >= 1024) {
+            byte = "TiB";
+            power /= 1024
+        }
+        let cost = ns.getPurchasedServerCost(power);
+        let como = ""; // add a M if over 1 mill
+        if (cost > 1000000) {
+            cost /= 1000000;
+            como = "M"
+        }
+        ns.tprint("Costs for a new Server with "+power+byte+" RAM: "+cost+como);
     }
     else {
-        ns.tprint("First Argument missing, or invalid. Use only an Integer at power of 2 (2,4,8,16...)");
+        ns.tprint("First Argument missing");
+        ns.tprint("Proper Use: run %filename %integer ");
+        ns.tprint("% integer has to be the log of estimated size ((2^ >>5<< ) = 32GB) ")
     }
 }
--- H3draut3r#6722/purchaseServer.js
+++ H3draut3r#6722/purchaseServer.js
@@ -1,16 +1,22 @@
 export async function main(ns) {
     if (ns.args[0] != null) {
-        if (ns.args[1] != null && Number.isInteger(ns.args[1]) && ns.args[1]%2==0) {
-            ns.purchaseServer(ns.args[0], ns.args[1]);
-            ns.tprint("Purchased a new Server. Hostname: "+ns.args[0]+" and "+ns.args[1]+"GB RAM")
+        if (ns.args[1] != null && Number.isInteger(ns.args[1]) && ns.args[1] >= 1) {
+            const power = Math.pow(2, ns.args[1]);
+            let byte = "GB";
+            if (power >= 1024) {
+                byte = "TiB";
+                power /= 1024
+            }
+            ns.purchaseServer(ns.args[0], power);
+            ns.tprint("Purchased a new Server. Hostname: " + ns.args[0] + " and " + power + byte+" RAM")
         }
         else {
-            ns.tprint("Second Argument missing or wrong (Integer, Power of 2)");
-            ns.tprint("Proper use of script: run purchaseServer.script %hostname %RAM");
+            ns.tprint("Second Argument missing or wrong");
+            ns.tprint("Proper Use: run %filename %hostname %integer ");
+            ns.tprint("% integer has to be the log of estimated size ((2^ >>5<< ) = 32GB) ")
         }
     }
     else {
-        ns.tprint("First Argument missing, proper use of the script:");
-        ns.tprint("run purchaseServer.script %hostname %RAM");
+        ns.tprint("Arguments missing");
     }
 }
