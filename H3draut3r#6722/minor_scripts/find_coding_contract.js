cdf8a43b3d09cf6926af990f2b982cea77a6623e
Hedrauta <H3draut3r@hotmail.de>
2021-11-24 02:06:57 +0100

    different kind of finding contracts

---
+++ H3draut3r#6722/minor_scripts/find_coding_contract.js
@@ -0,0 +1,33 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    // start fetching all Servers (Credits to skytos#2092)
+    function allServers(ns) {
+        const nodes = new Set
+        function dfs(node) {
+            nodes.add(node)
+            for (const neighbor of ns.scan(node)) {
+                if (!nodes.has(neighbor)) {
+                    dfs(neighbor)
+                }
+            }
+        }
+        dfs("home")
+        return [...nodes]
+    }
+    // filter for non-owned ones
+    function nos() { // ignore home and purchased server
+        let owned_servers = ["home"];
+        ns.getPurchasedServers().map(gps => owned_servers.push(gps)); 
+        return allServers(ns).filter(asf => owned_servers.indexOf(asf) < 0)
+    }
+
+    // search on every server
+    // first, empty entry for better clearance
+    ns.tprint("");
+    for (let server of nos()) {
+        let server_ls = ns.ls(server, ".cct")
+        if (server_ls.length >= 1) {
+            ns.tprint("Found " + server_ls.length + " Coding Contracts on " + server)
+        }
+    }
+}
