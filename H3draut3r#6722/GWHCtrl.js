49a30ffdeef089ed46dacf4fcfbc0e31136cf83c
Hedrauta <H3draut3r@hotmail.de>
2021-11-30 23:35:30 +0100

    fix typos, removed a few lines, + minor adjustment

--- H3draut3r#6722/GWHCtrl.js
+++ H3draut3r#6722/GWHCtrl.js
@@ -14,16 +14,8 @@ export async function main(ns) {
     ['debug', false]
   ]);
   let use_servers = [];
-  /* only enable with argument "--debug" if an alert occurs. Terminal will get spammed with alot of info, be sure max lenghts is high enough
-   in debug, there will be several lines "spammed" in the terminal:
-  > list of all script_servers process_list (idc the rest)
-  > action (grow or weak)
-  > sum of all threads running on that action and argument of target server
-  > is there an process with same arg running?
-  > sum of needed threads
-  > target server
-  if an alert is occuring, script does exits itself and contact me with the currents response on discord: H3draut3r#6722
-  */
+
+
   let hperct = 0.05;
   if (arg.hack >= 1 && arg.hack <= 100) {
     hperct = arg.hack / 100
@@ -160,7 +152,7 @@ export async function main(ns) {
     for (var srvscp of script_servers) {
       if (srvscp != cur_host) { // ignore current server for copy, bc scripts are already existent
         await ns.scp([sgname, swname, shname], cur_host, srvscp.name);
-        await ns.sleep(10)
+        await ns.sleep(1)
       }
     }
   }
@@ -173,9 +165,8 @@ export async function main(ns) {
   ns.tprint("Starting automatic Grow/Weaken/Hack");
   ns.tprint("Values set on startup: \n Hacking " + arg.hack + "% of targets Server money.\n Use non-owned rooted servers as a Script-Server (enable with --use_non_owned ): " + arg.use_non_owned + "\n Grow up to " + arg.grow_to + "% of targets max money");
   ns.tprint("INFO: Starting GWHCTRL on " + cur_host)
-
+  ns.disableLog("getServerUsedRam");
   if (!arg.debug) { // disable logging for certain functions (if debug is false), i do spam them alot 😂
-    ns.disableLog("getServerUsedRam");
     ns.disableLog("getServerMaxRam");
     ns.disableLog("sleep");
     ns.disableLog("getServerMinSecurityLevel");
@@ -186,8 +177,7 @@ export async function main(ns) {
     ns.disableLog("scp")
   }
 
-  let times = allServers(ns).map(am => { return { name: am, values: ns.getServer(am), } })
-  let wcount = 0;
+  let times = allServers(ns).map(am => { return { name: am } });
   // Script-part (in loop)
   while (1) {
     for (let t of times) {
@@ -197,9 +187,9 @@ export async function main(ns) {
       }
       t.wtime = ns.getWeakenTime(t.name)
     }
-    upd_ussrvr(); // includes if --use_all_purchased is set
+    upd_ussrvr(); // includes if --use_all_purchased or --use_non_owned is set
     upd_ssrvr();
-    await copy_files()
+    await copy_files();
     for (let tserv of nots()) {
       for (let scsrv of script_servers) {
         // updating script-servers
@@ -207,26 +197,28 @@ export async function main(ns) {
         update_process();
 
         // fetching a lot of data ++ redo on every script server
-        let current_money = ns.getServerMoneyAvailable(tserv.name);; // Targets Server Current money 
+        let current_money = ns.getServerMoneyAvailable(tserv.name); // Targets Server Current money 
         let max_money = tserv.values.moneyMax * gperct; // Targets Server Maximum money * grow to X % ... fixed value * mult
-        let growth_multi = Math.ceil(max_money / (current_money + 0.001)); // Targets Server 'Growth-multiplikator' 
+        let grwth_multi = Math.ceil(max_money / (current_money + 0.001)); // Targets Server 'Growth-multiplikator' 
         let current_security = ns.getServerSecurityLevel(tserv.name); // Targets Server current security
-        let min_security = tserv.values.minDifficulty // Targets Server minimum security ... fixed value
-        let grow_security = 0; // init
+        let min_security = tserv.values.minDifficulty; // Targets Server minimum security ... fixed value
+        let grw_security = 0; // init
         let sgt = calculateThreads(script_servers, sgname, tserv.name); // sum of growing threads for argument of target server (single-core)
         let swt = calculateThreads(script_servers, swname, tserv.name); // sum of weakning threads "" ""
         let sht = calculateThreads(script_servers, shname, tserv.name); // sum of hack threads "" ""
-        if (sgt > 0) { grow_sec = ns.growthAnalyzeSecurity(sgt) }; // if process_list isn't empty, calculate "grow of security"
+        if (sgt > 0) {  // if process_list isn't empty, calculate "grow of security"
+          grw_security = ns.growthAnalyzeSecurity(sgt)
+        }
         let sacwpr = threadSameArg(scsrv.process_list, swname, tserv.name); // *s*ame *a*rgument on *c*urrent servers *w*eaken *p*rocess *r*unning
         let sacgpr = threadSameArg(scsrv.process_list, sgname, tserv.name); // ...
         let sachpr = threadSameArg(scsrv.process_list, shname, tserv.name); // ......
-        let ncswt = Math.ceil(((current_security - min_security) / scsrv.w_res)); // needed current script-servers weaken-threads 
-        let nscwt = Math.ceil((ncswt / (1 + ((scsrv.values.cpuCores - 1) / 16)))); // needed single-core threads (if purchased servers will ever get multicore)
-        let ncsgt = Math.ceil(ns.growthAnalyze(tserv.name, growth_multi, scsrv.values.cpuCores)); // same as before, just for grow
-        let nscgt = Math.ceil(ncsgt / (1 + ((scsrv.values.cpuCores - 1) / 16))); //
+        let ncswt = Math.ceil((current_security - min_security) / scsrv.w_res); // needed current script-servers weaken-threads 
+        let ncwt = Math.ceil(ncswt / (1 + ((scsrv.values.cpuCores - 1) / 16))); // needed single-core threads (if purchased servers will ever get multicore)
+        let ncsgt = Math.ceil(ns.growthAnalyze(tserv.name, grwth_multi, scsrv.values.cpuCores)); // same as before, just for grow
+        let ncgt = Math.ceil(ncsgt / (1 + ((scsrv.values.cpuCores - 1) / 16))); //
         let ncsht = Math.ceil(ns.hackAnalyzeThreads(tserv.name, (current_money * hperct))); // needed threads, for X% of targets server money
-        let mwt = nscwt - swt; // missing weaken threads
-        let mgt = nscgt - sgt;
+        let mwt = ncwt - swt; // missing weaken threads
+        let mgt = ncgt - sgt;
         let mht = ncsht - sht;
         let sctp = threadPossible(scsrv, swname); // script-servers possible threads for weaken/grow
         let hcsctp = threadPossible(scsrv, shname); // "" for hack (it's 0.05GB less in size);
@@ -234,20 +226,20 @@ export async function main(ns) {
         // should be enough for now
 
         // run weaken first
-        if (current_security > min_security && mwt > 0 && !sacwpr) {
-          if (sctp == 0) {
+        if ((current_security + grw_security) > min_security && mwt > 0 && !sacwpr) {
+          if (sctp <= 0 || mwt <= 0) {
             await ns.sleep(1) // do nothing, bc there are no free threads on the script-server
           }
           else if (sctp >= mwt) {
             start(wname, scsrv.name, mwt, tserv.name);
-            ns.print("exec weaken, arg " + tserv.name + ", threads " + nwthreadscc + ", @ " + scsrv.name);
+            ns.print("exec weaken, arg " + tserv.name + ", threads " + mwt + ", @ " + scsrv.name);
             time_update.wstart = Date.now();
             time_update.havail = true;
             await ns.sleep(1) // prevent freeze
           }
           else if (mwt > sctp) {
             start(wname, scsrv.name, sctp, tserv.name);
-            ns.print("exec weaken, arg " + tserv.name + ", threads " + sctp + " missing threads (sc): " + (nscwt - sctp) + ", @" + scsrv.name);
+            ns.print("exec weaken, arg " + tserv.name + ", threads " + sctp + ", missing threads (sc): " + (ncwt - sctp) + ", @" + scsrv.name);
             await ns.sleep(1)
           }
           if (sctp > 0) {
@@ -255,10 +247,11 @@ export async function main(ns) {
             sctp = threadPossible(scsrv, swname)
           }
         }
+
         // run grow if needed
         if (current_money < max_money && mgt > 0 && !sacgpr) {
-          if (sctp == 0) {
-            ns.sleep(1) // 
+          if (sctp <= 0 || mgt <= 0) {
+            await ns.sleep(1) // 
           }
           else if (sctp >= mgt) {
             start(gname, scsrv.name, mgt, tserv.name);
@@ -268,7 +261,7 @@ export async function main(ns) {
           }
           else if (mgt > sctp) {
             start(gname, scsrv.name, sctp, tserv.name);
-            ns.print("exec grow, arg " + tserv.name + ", threads " + sctp + " missing threads (sc): " + (nscgt - sctp) + ", @" + scsrv.name);
+            ns.print("exec grow, arg " + tserv.name + ", threads " + sctp + ", missing threads (sc): " + (ncgt - sctp) + ", @" + scsrv.name);
             time_update.havail = false;
             await ns.sleep(1)
           }
@@ -280,11 +273,10 @@ export async function main(ns) {
 
         // get times asap, bc weaken/grow could cost some
         let current_time = Date.now();
-        let times_server = times.filter(tf => tf.name == tserv.name);
         let hack_time = ns.getHackTime(tserv.name);
         // run hack, if its in time of ending grow or servers are already at grow-percentage
-        if ((times_server.havail == true && (current_time - hack_time) > times_server.wstart + times_server.wtime) || (current_security <= min_security && current_money >= max_money)) {
-          if (sctp == 0) {
+        if (!sachpr && ((time_update.havail == true && (current_time - hack_time) > time_update.wstart + time_update.wtime) || (current_security <= min_security && current_money >= max_money))) {
+          if (hcsctp <= 0 || mht <= 0) {
             await ns.sleep(1)
           }
           else if (hcsctp >= mht) {
@@ -295,7 +287,7 @@ export async function main(ns) {
           }
           else if (mht > hcsctp) {
             start(hname, scsrv.name, hcsctp, tserv.name);
-            ns.print("exec hack, arg " + tserv.name + ", threads " + hcsctp + " missing threads (sc): " + (ncsht - hcsctp) + ", @" + scsrv.name);
+            ns.print("exec hack, arg " + tserv.name + ", threads " + hcsctp + ", missing threads (sc): " + (ncsht - hcsctp) + ", @" + scsrv.name);
             await ns.sleep(1)
           }
         } // end if hack
