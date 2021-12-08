700241584467498a4b121b943f0ea073dc3c3e31
H3draut3r <H3draut3r@hotmail.de>
2021-12-08 21:43:30 +0100

    still wip

--- H3draut3r#6722/minor_scripts/gen_IP.js
+++ H3draut3r#6722/minor_scripts/gen_IP.js
@@ -2,18 +2,47 @@ let testString = 5476;
 let stringArray = Array.from(String(testString), Number);
 let result = [];
 
+
+// if given string is 12 or 4 numbers long, the result is only 1 string
 function singleresult (string, length) {
-    let temp = true;
+    let convertIncomplete = true;
     let tempResult = [];
-    while (temp) {
+    while (convertIncomplete) {
         let tempConcat = string.splice(0, length/4).join("")
         tempConcat.concat(".");
         tempResult.push(tempConcat);
         if (string.length == 0){
-            temp = false
+            convertIncomplete = false
         }
     }
     return tempResult.join(".")
+}
+
+// for possible multi-results 
+function multipleResults (string){
+    let i,j,k;
+    i=j=k=1;
+    let tempResult = [];  // save correct arrays here
+    let tempCache = []; // for temporary check on result
+
+    while (i<4) {
+        let tempString = string; // edit only the temporary string
+        let tryConvert = true;
+        while (tryConvert){
+            if (tempString.length - i >= 10){
+                i++
+            }
+            if (tempString.length - i -j >= 7){
+                j++
+            }
+            if (tempString.length - i - j - k >= 4){
+                k++
+            }
+            else {
+                
+            }
+        }
+    }
 }
 if (stringArray.length == 12 || stringArray.length == 4) {
     result.push(singleresult(stringArray, stringArray.length))
