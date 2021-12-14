5b0a61e5017d568d00a2bb69602b56507530815c
Hedrauta <H3draut3r@hotmail.de>
2021-12-14 20:46:00 +0100

    Create test.js

---
+++ H3draut3r#6722/WIP/test.js
@@ -0,0 +1,8 @@
+let testdata = [99,68,153,103,138,107,35,190,128,89,132,50,34];
+console.log(stockTrader1(testdata));
+function stockTrader1(data) {
+    let i=0,j=1;
+    if (data[i]>data[j]) {
+        
+    }
+}
