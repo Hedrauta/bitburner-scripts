98ec890bc56d0ee5b58adc183c71517ce74b9566
Hedrauta <H3draut3r@hotmail.de>
2022-01-09 02:47:27 +0100

    save time before execute... than wait..... maybe it'll be gettin' faster <.<

--- ctrl_js/batch_grow.js
+++ ctrl_js/batch_grow.js
@@ -1,4 +1,5 @@
 export async function main(ns) {
+  let time = ns.getHackTime(ns.args[0])
   await ns.grow(ns.args[0])
-  await ns.sleep(ns.getHackTime(ns.args[0]))
+  await ns.sleep(time)
 }
--- ctrl_js/batch_hack.js
+++ ctrl_js/batch_hack.js
@@ -1,4 +1,5 @@
 export async function main(ns) {
+  let time = ns.getHackTime(ns.args[0])
   await ns.hack(ns.args[0])
-  await ns.sleep(ns.getHackTime(ns.args[0])*4)
+  await ns.sleep(time*4)
 }
